b1effcde44b42d5a292663e8bb5024009b9a3fc8
tonyclem <tonyclem001@gmail.com>
2022-10-22 22:55:10 +0200

    added search functionality

--- Backend/controllers/hotel.js
+++ Backend/controllers/hotel.js
@@ -47,7 +47,7 @@ export const getHotels = async (req, res, next) => {
   try {
     const hotels = await Hotel.find({
       ...others,
-      cheapestPrice: { $gt: min || 1, $lt: max || 999 },
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
     }).limit(req.query.limit);
     res.status(200).json(hotels);
   } catch (error) {
