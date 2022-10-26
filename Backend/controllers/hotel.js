57adccb7b44ed1cf1d0441c065fa78ce5690ecbb
tonyclem <tonyclem001@gmail.com>
2022-10-26 21:39:51 +0200

    added react context api, and modified client side and backend

--- Backend/controllers/hotel.js
+++ Backend/controllers/hotel.js
@@ -43,7 +43,7 @@ export const getHotel = async (req, res, next) => {
 };
 
 export const getHotels = async (req, res, next) => {
-  const { min, max, ...others } = req.params;
+  const { min, max, ...others } = req.query;
   try {
     const hotels = await Hotel.find({
       ...others,
